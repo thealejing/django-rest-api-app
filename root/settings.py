810fa4b3e369b4e1651d38d68e32cbb4a070ca6c
Alejandro Franco <alejing@gmail.com>
2022-10-29 16:18:22 -0500

    Todos los archivos static de nuevo

--- root/settings.py
+++ root/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
-#STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
